03baf2fd399a516299f68793db5c8c5625220e18
FELIPE DA SILVA CRUZ BRANDAO <felipe.scbrandao@senacsp.edu.br>
2024-10-07 17:17:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+/*let fruta = ["abacate" , "melão" , "kiwi", "maça"];
+console.log(fruta[0]);
+console.log(fruta[fruta.length -1]); // imprimindo o ultimo elemento array
+
+for(let i = 0; i < fruta.length; i++) {
+    console.log(fruta[i]);
+}
+    */
+
+/*let esportes = ["golf", "futebol", "basquete", "hockey"]
+for(let i = 0 ; i < esportes.length; i++){
+    console.log(esportes[i]);
+
+}
+*/
+
+// metodo map para criar um novo array que contem o  comprimento do array
+/*let novoCarros= carros.map((carros) => {
+   return carros.length;
+
+});
+console.log(novoCarros) // exibir o novo array
+*/
+//let legumes = ["batatas", "cenoura", "beterraba", " berinjela", "chuchu"]
+
+//let legumesComB = legumes.find(b=> b.startsWith("b"));
+
+//let legumesComB = legumes.filter(legumes =>legumes.startsWith("b"))
+
+//console.log(legumesComB)
+
+//function saudacao () {
+//   console.log("ola jovem");
+
+//}
+
+//saudacao();
+
+//function saudacao(nome) {
+  //console.log(`ola ${nome}`);
+//}
+//saudacao("joaquim");
+
+//modificar a funcao para que aceite um parametro nome e imprima uma mensagem personalizada.
+
+//function despedida(nome) {
+ //console.log(`ate logogo ${nome}`);
+//}
+ //despedida("joaquim");
+ //function soma (a, b ){
+//return a + b
+
+//}
+
+//console.log(soma(1, 2));
+//console.log(soma(4,9));
+//console.log((5,3));
+
+//criar uma funcao chamada multiplicacao que retorne o produto  e dois numeros
+
+/*function multiplicacao (a , b){
+    return a + b;
+    const resultado =a * b;
+    console.log(resultado);
+    return resultado;
+}
+multiplicacao(1, 2);
+
+let somaAnonima = function(a, b) {
+    return a + b
+
+}
+
+console.log(somaAnonima(1, 2));*/
+
+
+ 
+
+
